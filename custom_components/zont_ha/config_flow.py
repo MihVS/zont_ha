7e2bb3475e383feb5bdd1af23fad696e279792b2
Михаил Шутов <mihvs22@yandex.ru>
2023-12-29 12:17:32 +0300

    added config flow

--- custom_components/zont_ha/config_flow.py
+++ custom_components/zont_ha/config_flow.py
@@ -1,1 +1,13 @@
+from .const import DOMAIN
+from homeassistant import config_entries
+import voluptuous as vol
 
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            pass
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({vol.Required("password"): str})
+        )
