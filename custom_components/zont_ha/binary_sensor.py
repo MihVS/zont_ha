a738f697d846b046c0445001037b172860a8ca01
mihvs <mihvs22@gmail.com>
2025-12-06 00:19:21 +0300

    SENSOR: added sensor is_online device

--- custom_components/zont_ha/binary_sensor.py
+++ custom_components/zont_ha/binary_sensor.py
@@ -41,12 +41,65 @@ async def async_setup_entry(
                 unique_id = f'{entry_id}{device.id}{control_status.id}'
                 binary_sensors.append(ZontBinarySensorControl(
                     coordinator, device, control_status, unique_id))
+        binary_sensors.append(ZontOnlineBinarySensor(
+            coordinator, device, unique_id=f'{entry_id}{device.id}_online'
+        ))
         for binary_sensor in binary_sensors:
             hass.data[DOMAIN][CURRENT_ENTITY_IDS][entry_id].append(
                 binary_sensor.unique_id)
         if binary_sensors:
             async_add_entities(binary_sensors)
             _LOGGER.debug(f'Добавлены бинарные сенсоры: {binary_sensors}')
+
+
+class ZontOnlineBinarySensor(CoordinatorEntity, BinarySensorEntity):
+
+    def __init__(
+            self, coordinator: ZontCoordinator, device: DeviceZONT,
+            unique_id: str
+    ) -> None:
+        super().__init__(coordinator)
+        self._zont: Zont = coordinator.zont
+        self._device: DeviceZONT = device
+        self._unique_id: str = unique_id
+        self._attr_device_info = coordinator.devices_info(device.id)
+
+    @property
+    def name(self) -> str:
+        return f'{self._device.name}_online'
+
+    @property
+    def unique_id(self) -> str:
+        return self._unique_id
+
+    @property
+    def device_class(self) -> BinarySensorDeviceClass | None:
+        """Return the class of this entity."""
+        return BinarySensorDeviceClass.CONNECTIVITY
+
+    @property
+    def is_on(self) -> bool | None:
+        """Return true if the binary sensor is on."""
+        return self._device.online
+
+    def __repr__(self) -> str:
+        if not self.hass:
+            return f"<Binary sensor entity {self.name}>"
+        return super().__repr__()
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Обработка обновлённых данных от координатора"""
+
+        device = self.coordinator.zont.get_device(self._device.id)
+        if device.online != self._device.online:
+            _LOGGER.debug(
+                f'Состояние устройства "{self._device.name}" '
+                f'обновилось с {self._device.online} на {device.online}')
+        self._device.online = device.online
+        if not device.online:
+            _LOGGER.warning(f'Устройство {self._device.name} недоступно!')
+        self.async_write_ha_state()
 
 
 class ZontBinarySensor(CoordinatorEntity, BinarySensorEntity):
